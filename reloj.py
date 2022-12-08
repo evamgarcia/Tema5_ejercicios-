192f7be3fa8d6c3da508a18780cb3b028bca1758
evamgarcia <emgarcia2911@gmail.com>
2022-12-08 19:15:29 +0100

    reloj

---
+++ reloj.py
@@ -0,0 +1,18 @@
+#Ejercicio 1
+# ¿Eres capaz de desarrollar un reloj de horas, minutos y segundos utilizando el módulo datetime con la hora actual? 
+# Hazlo en un script llamado reloj.py y ejecútalo en la terminal:
+# Ayuda
+# El módulo time integra una función llamada sleep(segundos) que puede pausar la ejecución 
+# de un programa durante un tiempo. Así mismo el módulo os integra la función system('cls') 
+# y system('clear') para limpiar la pantalla de la terminal en sistemas Windows y Linux/Unix respecticamente.
+
+import datetime 
+import time
+import os 
+
+while (True):
+    os.system('cls')
+    dt = datetime.datetime.now()
+    print("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
+    time.sleep(1)
+    
